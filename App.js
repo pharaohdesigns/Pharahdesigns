b5ebdcdf111ed1e8cb62a30c707a2ded41f4e49f
pharaohdesigns <hanesdominic93@gmail.com>
2026-06-01 19:16:38 -0400

    Create App.js

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello Dominic 👋 Your Expo app is running!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#111',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 22,
+    fontWeight: '600',
+  },
+});
